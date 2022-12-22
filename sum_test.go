529d49ca84fa90004c36a752c3d121eb3b042e07
Igor Neves Faustino <igor@nfaustino.com>
2022-12-22 16:34:02 -0300

    ci: pushing a fail commit

--- sum_test.go
+++ sum_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := sum(15, 15)
+	total := sum(15, 14)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
