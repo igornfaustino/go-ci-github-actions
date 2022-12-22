9cd0edab8195706c6b1995c039d1c80eb06b55a0
Igor Neves Faustino <igor@nfaustino.com>
2022-12-22 16:34:52 -0300

    fix: failed test

--- sum_test.go
+++ sum_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := sum(15, 14)
+	total := sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
